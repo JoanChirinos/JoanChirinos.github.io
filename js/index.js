86e36ffc5da9a3d1ac6f8e0d2cf6228246fc3736
Joan Chirinos <jchirinos3201@gmail.com>
2021-12-07 04:00:20 -0500

    Finish v1 of the index

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let nav = document.getElementById('navbar');
+
+document.onscroll = function() {
+  var scrollTop = document.documentElement.scrollTop;
+//  console.log(scrollTop);
+  
+  if (scrollTop == 0) {
+    if (nav.classList.contains('to-grey')) {
+      nav.classList.remove('to-grey');
+    }
+    if (!nav.classList.contains('to-blue')) {
+      nav.classList.add('to-blue');
+    }
+  }
+  else {
+    if (nav.classList.contains('to-blue')) {
+      nav.classList.remove('to-blue');
+    }
+    if (!nav.classList.contains('to-grey')) {
+      nav.classList.add('to-grey');
+    }
+  }
+}
